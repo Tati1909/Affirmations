ea6ec9455eb13e016d1fc59ba1172384c871add5
татьяна <emuart155@gmail.com>
2021-07-03 16:18:23 +0300

    Добавила картинки и значок приложения.

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,4 +1,12 @@
 package com.example.affirmations.model
 
-data class Affirmation(val stringResourceId: Int) {
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+//Добавляем аннотации, т к можно случайно передать аргументы в неправильном порядке
+// ( imageResourceId сначала вместо stringResourceId)
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+) {
 }
